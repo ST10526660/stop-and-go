dda10a1ce3b3072b6efe683aad5d4728a87a4852
ST10526660 <ST10526660@myemeris.edu.za>
2026-02-25 14:42:06 +0200

    Final submission

---
+++ src/main/java/com/mycompany/stopandgo/Stopandgo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Stopandgo {
+
+    public static void main(String[] args) {
+//        displaying a stop and go sign using "asterix" and "X's"
+        System.out.println("             ----------\n");
+
+        System.out.println("       ****            XXXXXXXX");
+        System.out.println("     *      *          X      X");
+        System.out.println("    *  STOP  *         X  GO  X");
+        System.out.println("     *      *          X      X");
+        System.out.println("       ****            XXXXXXXX");
+        System.out.println("        **                XX");
+        System.out.println("        **                XX");
+        System.out.println("        **                XX");
+        System.out.println("        **                XX");
+        System.out.println("        **               XXXX");;
+    }
+}
